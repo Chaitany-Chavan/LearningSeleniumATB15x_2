31ba27b70c71a6f8f0263ebd57c7d0904dd16b1e
Chaitany_Chavan <chaitanychavan993@gmail.com>
2026-02-21 17:12:28 +0530

    21 feb 2026

---
+++ src/test/java/com/thetestingacademy/TestSelenium07_Selenium_Commands.java
@@ -0,0 +1,8 @@
+package com.thetestingacademy;
+
+public class TestSelenium07_Selenium_Commands {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/test/java/com/thetestingacademy/selenium002.java
@@ -0,0 +1,11 @@
+package com.thetestingacademy;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class selenium002 {
+    public static void main(String[] args) {
+        FirefoxDriver driver = new FirefoxDriver();
+        driver.get("https://sdet.live");
+        driver.quit();
+    }
+}
